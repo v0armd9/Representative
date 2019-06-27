8f273f8451e6a750b0872d7ea746b0ed13e52226
Darin Armstrong <darin.marmstrong@gmail.com>
2019-06-27 14:08:31 -0600

    Created Model and structed file Heirarchy

---
+++ Representative/Models/Representative.swift
@@ -0,0 +1,23 @@
+//
+//  Representative.swift
+//  Representative
+//
+//  Created by Darin Marcus Armstrong on 6/27/19.
+//  Copyright © 2019 Darin Marcus Armstrong. All rights reserved.
+//
+
+import Foundation
+
+struct TopLevelJSON: Codable {
+    let results: [Results]
+}
+
+struct Results: Codable {
+    let name: String
+    let party: String
+    let state: String
+    let district: String
+    let phone: String
+    let office: String
+    let link: String
+}
